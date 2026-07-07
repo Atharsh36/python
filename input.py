c860a06e8ec38b888888904dd772f5c33f0c8e37
Atharsh36 <atharsh605@gmail.com>
2026-07-07 14:26:07 +0530

    first commit

---
+++ input.py
@@ -0,0 +1,11 @@
+a =int(input("enter number 1:"))
+b = int(input("enter number 2:"))
+
+print("Addition:",a+b)
+print("subraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor division:",a//b)
+print("modulus:",a%b)
+print("expoonent:",a**b)
+
